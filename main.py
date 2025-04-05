cb7639b76c33011e389ae2b67bed6533ca2efaf2
Marcel Lefebvre <marcel.lefebvre01@gmail.com>
2025-04-05 14:02:02 -0400

    refactor functions

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+from stats import get_num_words
+
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return (f.read())
+
+
+def main():
+    count = get_num_words(get_book_text("books/frankenstein.txt"))
+    print(f"{count} words found in the document")
+
+
+main()
